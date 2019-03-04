76ce2381cc104a5edc2c542c8d166ce60aba777f
porkeater <248207330@qq.com>
2019-03-04 12:19:43 +0800

    userdao

--- src/com/dao/UserDao.java
+++ src/com/dao/UserDao.java
@@ -1,5 +1,47 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.entity.User;
+import com.mysql.jdbc.PreparedStatement;
+import com.util.JDBCUtil;
+import com.util.StringUtils;
+
 public class UserDao {
 
+	/**
+	 * 插入用户
+	 * @param user
+	 */
+	public void insertUser(User user) {
+		String insertUser = "insert into user (user_name,password,email) values (?,?,?)";
+		//用户名
+		String userName = user.getUserName();
+		//密码
+		String password = user.getPassword();
+		//为密码加密
+		try {
+			password = StringUtils.encrypt(password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//邮箱
+		String email = user.getEmail();
+		//执行sql
+		try {
+			Connection conn = JDBCUtil.getConn();
+			PreparedStatement preparedStatement = (PreparedStatement) conn.prepareStatement(insertUser);
+			preparedStatement.setString(1, userName);
+			preparedStatement.setString(2, password);
+			preparedStatement.setString(3, email);
+			int i = preparedStatement.executeUpdate();
+			JDBCUtil.close(conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
--- src/com/test/test.java
+++ src/com/test/test.java
@@ -1,37 +1,28 @@
 package com.test;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import com.dao.UserDao;
 import com.entity.User;
-import com.mysql.jdbc.PreparedStatement;
-import com.util.HibernateUtil;
-import com.util.JDBCUtil;
+import com.util.StringUtils;
+
 
 @Component
 @Repository
 public class test {
 	@Autowired
 	private static SessionFactory sessionFactory;
-
+	
 	public static void main(String[] args) {
-		try {
-			Connection con = JDBCUtil.getConn();
-			String a = "insert into User(user_name) values ('aaaa')";
-			PreparedStatement preparedStatement = (PreparedStatement) con.prepareStatement(a);
-			int i = preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		UserDao dao = new UserDao();
+		User user = new User();
+		user.setUserName("123");
+		user.setPassword("asd");
+		user.setEmail("asd");
+		dao.insertUser(user);
 	}
 
 }
